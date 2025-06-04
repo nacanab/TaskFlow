989c1ff9d80b6925c6721bbe21cd1614b3fafe17
Abdel_Laif_NACANABO <151642832+nacanab@users.noreply.github.com>
2025-06-04 19:18:47 +0000

    Changement des fichiers du dossier mobile

--- mobile/taskflow/lib/core/network/endpoints.dart
+++
@@ -1,9 +0,0 @@
-class Endpoints {
-  static const String baseUrl = 'http://127.0.0.1/api/v1';
-
-  // Pour l'authentification
-  static const String login = '$baseUrl/login';
-  static const String register = '$baseUrl/register';
-  static const String logout = '$baseUrl/logout';
-  static const String currentUser = '$baseUrl/users/get_user';
-}
